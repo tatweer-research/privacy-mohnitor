8c237d5cb243bc53e762050bae224cac4a33754a
Mohammad.Al-Zoubi <mohammad.al-zoubi@samdsolutions.de>
2023-06-24 08:24:09 +0200

    Created the HuggingFace policy_detection dataset

--- instruction_finetuning/data_preprocessing/README.md
+++
@@ -1,3 +0,0 @@
-## PrivacyGLUE
-
-The instruction finetuning is based on the [PrivacyGLUE](https://github.com/infsys-lab/privacy-glue) dataset proposed by [Shankar et al.](https://www.mdpi.com/2076-3417/13/6/3701).
---
+++ instruction_finetuning/data_preprocessing/policy_detection.py
@@ -0,0 +1,55 @@
+import os
+
+import datasets
+import pandas as pd
+
+LABELS = ["Not Policy", "Policy"]
+
+
+def load_policy_detection(directory: str) -> datasets.DatasetDict:
+    # initialize DatasetDict object
+    combined = datasets.DatasetDict()
+
+    # read csv file and choose subset of columns
+    df = pd.read_csv(os.path.join(directory, "1301_dataset.csv"), index_col=0)
+    df = df[["policy_text", "is_policy"]]
+
+    # replace labels from boolean to strings for consistency
+    df["is_policy"] = df["is_policy"].replace({True: "Policy", False: "Not Policy"})
+
+    # rename columns for consistency
+    df = df.rename(columns={"policy_text": "text", "is_policy": "label"})
+
+    # convert into HF datasets
+    dataset = datasets.Dataset.from_pandas(df, preserve_index=False)
+    label_info = datasets.ClassLabel(names=LABELS)
+
+    # make split using HF datasets internal methods
+    train_test_dataset_dict = dataset.train_test_split(test_size=0.3, seed=42)
+    train_valid_dataset_dict = train_test_dataset_dict["train"].train_test_split(
+        test_size=0.15, seed=42
+    )
+
+    # manually assign them to another DatasetDict
+    combined["train"] = train_valid_dataset_dict["train"]
+    combined["validation"] = train_valid_dataset_dict["test"]
+    combined["test"] = train_test_dataset_dict["test"]
+
+    # collect and distribute information about label
+    for split in ["train", "validation", "test"]:
+        combined[split] = combined[split].map(
+            lambda examples: {
+                "label": [label_info.str2int(label) for label in examples["label"]]
+            },
+            batched=True,
+        )
+        combined[split].features["label"] = label_info
+
+    return combined
+
+
+if __name__ == "__main__":
+    directory = r"C:\Users\Mohammad.Al-zoubi\Documents\projects\privacy-mohnitor\instruction_finetuning\data" \
+                r"\policy_detection"
+    dataset_dict = load_policy_detection(directory)
+    dataset_dict.push_to_hub('alzoubi36/policy_detection')
