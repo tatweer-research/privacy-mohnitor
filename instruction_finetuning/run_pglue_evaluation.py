ea9fe30e1071a8e58ec566166f8264f33589ef2c
Mohammad.Al-Zoubi <mohammad.al-zoubi@samdsolutions.de>
2023-09-07 15:19:29 +0200

    wrote evaluation functions

--- instruction_finetuning/run_pglue_evaluation.py
+++ instruction_finetuning/run_pglue_evaluation.py
@@ -1,39 +1,38 @@
+import json
+import time
+from pathlib import Path
+
+import jax.numpy as jnp
 from transformers import (
     AutoTokenizer,
     T5ForConditionalGeneration,
     FlaxT5ForConditionalGeneration,
 )
 
-import jax.numpy as jnp
-import time
-import json
-from pathlib import Path
+from instruction_finetuning.data_preprocessing import text2text_functions
+from tqdm import tqdm
 
-from instruction_finetuning.data_preprocessing import text2text_functions, from_text_functions
-
-
-FLAX = True
-PT = False
+FLAX = False
+PT = True
 
 
-# some computation...
 def initialize_model(model_name):
     tokenizer = AutoTokenizer.from_pretrained("t5-small", use_fast=True)
     models = {'flax': '', 'pt': ''}
     if FLAX:
         models['flax'] = FlaxT5ForConditionalGeneration.from_pretrained(model_name,
-                                                               dtype=getattr(jnp, "float32"),
-                                                               seed=42)
+                                                                        dtype=getattr(jnp, "float32"),
+                                                                        seed=42)
     if PT:
         models['pt'] = T5ForConditionalGeneration.from_pretrained(model_name, from_flax=True).to('cpu')
     return tokenizer, models
 
 
-def generate_model_outputs(models, tokenizer, input_text):
+def generate_model_outputs(models, tokenizer, input_texts):
     results = {'flax': "", 'pt': ""}
 
     if FLAX:
-        inputs = tokenizer(input_text,
+        inputs = tokenizer(input_texts,
                            padding="max_length",
                            max_length=512,
                            truncation=True,
@@ -49,7 +48,7 @@ def generate_model_outputs(models, tokenizer, input_text):
         results['flax'] = tokenizer.batch_decode(output_sequences, skip_special_tokens=True,
                                                  clean_up_tokenization_spaces=False)
     if PT:
-        inputs = tokenizer(input_text,
+        inputs = tokenizer(input_texts,
                            max_length=512,
                            padding="max_length",
                            truncation=True,
@@ -63,40 +62,46 @@ def generate_model_outputs(models, tokenizer, input_text):
             max_length=512,
         )
 
-        results['pt'] = tokenizer.batch_decode(output_sequences, skip_special_tokens=False)
+        results['pt'] = tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
     return results
 
 
-def generate_model_outputs_dataset(models, tokenizer, pglue_task="privacy_qa"):
+def generate_model_outputs_dataset(models, tokenizer, pglue_task="opp_115"):
     dataset_dict = text2text_functions["privacy_glue"][pglue_task]()
 
     DATASET = 'test'
     inputs = [dataset_dict[DATASET][i]['text'] for i in range(len(dataset_dict[DATASET]))]
-    # inputs = [dataset_dict[DATASET][i]['text'] for i in range(32)]
     print("Generating model outputs for {} examples".format(len(inputs)))
-    outputs = generate_model_outputs(models, tokenizer, inputs)
+
+    # Split inputs into batches of a specified size (e.g., batch_size)
+    batch_size = 32  # You can adjust this as needed
+    all_outputs = {'flax': [], 'pt': []}
+    for i in tqdm(range(0, len(inputs), batch_size)):
+        batch_inputs = inputs[i:i + batch_size]
+        outputs = generate_model_outputs(models, tokenizer, batch_inputs)
+        if outputs['flax']:
+            all_outputs['flax'] += outputs['flax']
+        if outputs['pt']:
+            all_outputs['pt'] += outputs['pt']
+        # Save or process the batch outputs here
+
+        # Print the batch results
+        # for j, input_text in enumerate(batch_inputs):
+        #     print("-" * 20 + "Batch: {} | Example: {} ".format(i // batch_size, j) + "-" * 20)
+        #     print("Input: ", input_text)
+            # print("flax: ", outputs['flax'][j])
+            # print("pt: ", outputs['pt'][j])
     results_path = Path(model_name).parent / "results.json"
     with open(results_path, 'w', encoding="utf-8") as f:
-        json.dump(outputs, f, ensure_ascii=False, indent=4)
+        json.dump(all_outputs, f, ensure_ascii=False, indent=4)
 
-    for i in range(20):
-        print("-" * 20 + "model input" + "-" * 20)
-        input = inputs[i]
-        print(input)
-        print("-" * 20 + "original labels" + "-" * 20)
-        labels = [dataset_dict[DATASET][i]['label']]
-        print(labels)
-        print("-" * 20 + "model output" + "-" * 20)
-        print("flax: ", outputs['flax'][i])
-        print("pt: ", outputs['pt'][i])
     print()
 
 
 if __name__ == '__main__':
-    task_prefix = "translate English to German: "
-    # use different length sentences to test batching
-    sentences = ["The house is wonderful.", "I like to work in NYC."]
-    model_name = "/home/Mohammad.Al-Zoubi/privacy-mohnitor/instruction_finetuning/experiments/2023-09-03/privacy_qa/best_model/"
+
+    model_name = "/home/Mohammad.Al-Zoubi/privacy-mohnitor/instruction_finetuning/experiments/2023-09-03" \
+                 "/opp_115/best_model/"
 
     tokenizer, models = initialize_model(model_name)
 
