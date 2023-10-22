3415d339342655d756382ec00ef9edfd81faeb7d
Mohammad.Al-Zoubi <mohammad.al-zoubi@samdsolutions.de>
2023-10-22 12:30:56 +0200

    Refactoring and multitask learning

--- instruction_finetuning/models_evaluation/pipeline.py
+++ instruction_finetuning/models_evaluation/pipeline.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from instruction_finetuning.models_evaluation import (
     list_models,
@@ -15,6 +16,9 @@ from pathlib import Path
 import yaml
 from pydantic import BaseModel
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 class GeneralConfig(BaseModel):
     remote_user: str
@@ -24,6 +28,7 @@ class GeneralConfig(BaseModel):
     requested_sizes: list
     requested_base_models: list
     requested_tasks: list
+    examples_limit: int
 
 
 class AppConfig(BaseModel):
@@ -43,6 +48,7 @@ def parse_config(yaml_config):
 class T5EvaluationPipeline:
     def __init__(self, config):
         self.general_config = config.general
+        logger.info(f"Current configuration:\n{json.dumps(self.general_config.__dict__, indent=4)}")
         self.results = {}
 
     @staticmethod
@@ -57,9 +63,9 @@ class T5EvaluationPipeline:
 
             # Execute the command using subprocess
             subprocess.run(command, shell=True, check=True)
-            print("Hugging Face cache cleared successfully.")
+            logger.info("Hugging Face cache cleared successfully.")
         except subprocess.CalledProcessError as e:
-            print(f"Error clearing Hugging Face cache: {e}")
+            logger.info(f"Error clearing Hugging Face cache: {e}")
 
     @staticmethod
     def pglue_model(model_name):
@@ -88,7 +94,7 @@ class T5EvaluationPipeline:
         return model_names
 
     def evaluate_model(self, model_name, task):
-        print(f"Running inference on model {model_name} with task {task}...")
+        logger.info(f"Running inference on model {model_name} with task {task}...")
 
         model_save_name = model_name
 
@@ -101,18 +107,18 @@ class T5EvaluationPipeline:
 
         tokenizer_name = self.get_tokenizer_name(model_name)
         max_generation_length = 5 if task == 'privacy_qa' else 512
-        print(f"Limiting generation length on model {model_name} to {max_generation_length} tokens...")
+        logger.info(f"Limiting generation length on model {model_name} to {max_generation_length} tokens...")
         try:
             model_outputs_dir = Path(self.general_config.path_to_model_outputs).joinpath(model_save_name)
             model_outputs_json = model_outputs_dir / 'outputs.json'
             if model_outputs_json.exists():
-                print(f"Model {model_name} already evaluated. Skipping...")
+                logger.info(f"Model {model_name} already evaluated. Skipping...")
                 return None
 
             model_outputs_dir.mkdir(parents=True, exist_ok=True)
             evaluation_result = generate_and_evaluate(model_name=model_name,
                                                       batch_size=self.get_batch_size(model_name),
-                                                      examples_limit=None,
+                                                      examples_limit=self.general_config.examples_limit,
                                                       tokenizer_name=self.get_tokenizer_name(model_name),
                                                       pglue_task=task,
                                                       split='test',
@@ -124,18 +130,18 @@ class T5EvaluationPipeline:
                 json.dump(self.results, f, ensure_ascii=False, indent=4)
 
         except Exception as e:
-            print(f"Error running inference on model {model_name}: {e}")
-            print(f"Batch size: {self.get_batch_size(model_name)}")
-            print(f"Tokenizer name: {tokenizer_name}")
-            print(f"Task: {get_task_name(model_name)}")
-            print("Skipping model...")
+            logger.info(f"Error running inference on model {model_name}:\n{e}")
+            logger.info(f"Batch size: {self.get_batch_size(model_name)}")
+            logger.info(f"Tokenizer name: {tokenizer_name}")
+            logger.info(f"Task: {get_task_name(model_name)}")
+            logger.info("Skipping model...")
 
     def run(self):
         available_models = list_models(huggingface_search_params)
         model_names = [model['modelId'] for model in available_models]
         model_names = self.filter_models(model_names)
 
-        print(f"Found {len(model_names)} models...")
+        logger.info(f"Found {len(model_names)} models...")
         for model_name in model_names:
 
             task = get_task_name(model_name)
